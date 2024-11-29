f3713f805c04aabf5c8f6d78f63b74ca250647e9
argonaultes <gael@argonaultes.fr>
2024-11-29 15:38:59 +0100

    reorg server and sqlalchemy package

--- server/Application.py
+++ server/Application.py
@@ -10,14 +10,6 @@ class Application:
 
     def __init__(self):
         self.reload()
-        self.__actions = {
-            "1": self.display_books,
-            "2": self.buy_book,
-            "2a": self.add_book_in_bookshop,
-            "3": self.save_library,
-            "4": self.reload_library,
-            "5": self.exit_program,
-        }
 
     def add_book_in_bookshop(self, book):
         self.__bookshop.add_book(jsonpickle.decode(book))
@@ -54,39 +46,3 @@ class Application:
         except ValueError:
             return ("Entrée invalide. Veuillez entrer un numéro.")
 
-    def save_library(self):
-        self.save_in_json()
-        print(f"Librairie enregistrée dans '{self.__json_file}'.")
-
-    def reload_library(self):
-        if self.reload():
-            print(f"Librairie rechargée depuis '{self.__json_file}'.")
-
-    def exit_program(self):
-        print("À bientôt !")
-        exit()
-
-    def main(self):
-        self.reload()
-
-        
-
-        while True:
-            print("\n=== Menu Principal ===")
-            print("1. Afficher les livres")
-            print("2. Acheter un livre")
-            print("2a. Créer un livre")
-            print("3. Enregistrer la librairie")
-            print("4. Recharger la librairie")
-            print("5. Quitter")
-            print("=======================")
-            choix = input("Entrez votre choix : ")
-
-            action = self.__actions.get(choix)
-            if action:
-                action()  # Appel de la fonction associée
-            else:
-                print("Veuillez sélectionner une option entre 1 et 5.")
-
-if __name__ == "__main__":
-    Application().main()
--- server/socket_server.py
+++ server/socket_server.py
@@ -1,6 +1,5 @@
 from Application import Application
 import socketserver
-import json
 import jsonpickle
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -15,23 +14,18 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print("Received from {}:".format(self.client_address[0]))
         method, params = self.data.decode('utf-8').split('//')
         application = Application()
         actions = {
             "display_books": application.display_books,
             "buy_book": application.buy_book,
             "add_book_in_bookshop": application.add_book_in_bookshop,
-            "3": application.save_library,
-            "4": application.reload_library,
-            "5": application.exit_program,
         }
         action = actions.get(method)
-        print(params)
         if action:
             response = action(params)  # Appel de la fonction associée
         else:
-            response = "Veuillez sélectionner une option entre 1 et 5."
+            response = f"Veuillez sélectionner une option entre 1 et {len(actions)}."
         print(jsonpickle.encode(response).encode('utf-8'))
         # just send back the same data, but upper-cased
         self.request.sendall( jsonpickle.encode(response).encode('utf-8') )
